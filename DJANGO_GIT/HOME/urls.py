e99e4a93824338bef93ca0b1930c9a53e053505c
Evans <okemwa.ramimi@gmail.com>
2023-06-10 17:39:28 +0300

    changes

--- DJANGO_GIT/HOME/urls.py
+++ DJANGO_GIT/HOME/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home.html")
+    path('', views.home, name="Home page"),
+    path('about/', views.about, name="About"),
+    path('contacts/', views.contacts, name="Contacts"),
+    path('services/', views.services, name="Services")
 ]
